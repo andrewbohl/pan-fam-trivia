bbd8ae24797f6036f97168894b9a3bcc81e539af
Andrew <abohl777@gmail.com>
2020-08-24 20:19:39 -0500

    add index page

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+function index() {
+  return (
+    <Fragment>
+      <div> This is where all the cool stuff is going to go</div>
+    </Fragment>
+  );
+}
+
+export default index;
