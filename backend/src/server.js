55bbe192245747ed08167fea45f1399935188101
Andrew <abohl777@gmail.com>
2020-09-08 21:13:00 -0500

    backend linting

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,31 +1,28 @@
-const express = require('express');
-const http = require('http');
-const socketio = require('socket.io');
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const app = express();
-const index = require('./routes/index');
+const index = require("./routes/index");
 app.use(index);
 
 const server = http.createServer(app);
 
 const io = socketio(server);
 
-
 // Run when client connects
-io.on('connection', socket => {
-    console.log('new client connection')
+io.on("connection", (socket) => {
+  console.log("new client connection");
 
-    socket.on('message', () => {
-        console.log('a message sent');
-    })
+  socket.on("message", () => {
+    console.log("a message sent");
+  });
 
-    socket.on("disconnect", () => {
-        console.log("Client disconnected")
-    });
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
 });
-
-
 
 server.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
